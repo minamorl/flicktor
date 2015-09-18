aa79540477425a18deab1cf315944c384f765035
minamorl <minamorl@users.noreply.github.com>
2015-09-19 03:13:45 +0900

    refactor

--- tw/__main__.py
+++ tw/__main__.py
@@ -7,8 +7,7 @@ import os
 def main():
     api = staccato.startup()
 
-    conf = import_configurations("~/.staccato.conf")
-    conf = conf['OAuth1Settings']
+    conf = import_configurations("~/.staccato.conf")['OAuth1Settings']
     api.auth(conf["CONSUMER_KEY"], conf["CONSUMER_SECRET"], conf["ACCESS_TOKEN_KEY"], conf["ACCESS_TOKEN_SECRET"])
     api.statuses_update(status=sys.argv[1])
 
