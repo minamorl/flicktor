5916b15af5f1577e7ef057a493e4e6e89904c520
minamorl <minamorl@users.noreply.github.com>
2015-09-19 02:52:43 +0900

    initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tw",
+    version="0.0.1",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': ['tw = tw.__main__:main']
+    },
+    author='minamorl',
+    author_email='minamorl@users.noreply.github.com',
+    install_requires=[
+        'staccato',
+    ],
+)
---
+++ tw/__main__.py
@@ -0,0 +1,22 @@
+import staccato
+import configparser
+import sys
+import os
+
+
+def main():
+    api = staccato.startup()
+
+    conf = import_configurations("~/.staccato.conf")
+    conf = conf['OAuth1Settings']
+    api.auth(conf["CONSUMER_KEY"], conf["CONSUMER_SECRET"], conf["ACCESS_TOKEN_KEY"], conf["ACCESS_TOKEN_SECRET"])
+    api.statuses_update(status=sys.argv[1])
+
+
+def import_configurations(path):
+    config = configparser.ConfigParser()
+    config.read(os.path.expanduser(path))
+    return config
+
+if __name__ == '__main__':
+    main()
