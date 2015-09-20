997806f2ec7893bfe0d8044d0a6861555e697b17
minamorl <minamorl@users.noreply.github.com>
2015-09-20 19:08:46 +0900

    feat: "say" subcommand

--- tw/__main__.py
+++ tw/__main__.py
@@ -1,21 +1,40 @@
 import staccato
+import argparse
 import configparser
 import sys
 import os
+import functools
 
 
 def main():
-    api = staccato.startup()
+    parser = _argpaser()
+    args = parser.parse_args()
+    args.func(args)
 
+
+@functools.lru_cache()
+def _api():
     conf = import_configurations("~/.staccato.conf")['OAuth1Settings']
+    api = staccato.startup()
     api.auth(conf["CONSUMER_KEY"], conf["CONSUMER_SECRET"], conf["ACCESS_TOKEN_KEY"], conf["ACCESS_TOKEN_SECRET"])
-    api.statuses_update(status=sys.argv[1])
+    return api
 
+def subcommand_say(args):
+    _api().statuses_update(status=args.status)
 
 def import_configurations(path):
     config = configparser.ConfigParser()
     config.read(os.path.expanduser(path))
     return config
+
+def _argpaser():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+    subparser_say = subparsers.add_parser('say')
+    subparser_say.add_argument('status', )
+    subparser_say.set_defaults(func=subcommand_say)
+
+    return parser
 
 if __name__ == '__main__':
     main()
