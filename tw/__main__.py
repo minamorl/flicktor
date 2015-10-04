1b759d8d92fdf19933cc58ce7e768a4a2d253eb8
minamorl <minamorl@users.noreply.github.com>
2015-10-04 11:55:57 +0900

    fix: show usage if passing arguments are not correct.

--- tw/__main__.py
+++ tw/__main__.py
@@ -4,7 +4,10 @@ from tw import subcommands
 def main():
     parser = subcommands._argpaser()
     args = parser.parse_args()
-    args.func(args)
+    try: 
+        args.func(args)
+    except AttributeError:
+        parser.print_help()
 
 
 if __name__ == '__main__':
