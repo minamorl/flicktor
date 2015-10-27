0743a951e9f3df2acce250d7c6282150bd402624
minamorl <minamorl@users.noreply.github.com>
2015-10-28 03:37:57 +0900

    fix name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.0.1",
     packages=find_packages(),
     entry_points={
-        'console_scripts': ['tw = tw.__main__:main']
+        'console_scripts': ['tw = flicktor.__main__:main']
     },
     author='minamorl',
     author_email='minamorl@users.noreply.github.com',
